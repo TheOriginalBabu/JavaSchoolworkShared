3ef5f32f8b89a9c7177bddddc55189bf8f001017
Griffin Robinson <griffrob1205@gmail.com>
2023-04-21 14:08:33 -0400

    Update Deck.java

--- IzaanGriffinClassesLab/src/PokerHandSimulator/Deck.java
+++ IzaanGriffinClassesLab/src/PokerHandSimulator/Deck.java
@@ -1,7 +1,5 @@
 package classintro.PokerHandSimulator;
 
-import java.util.Random;
-
 public class Deck {
 
     public Card[] deck = new Card[52];
@@ -9,7 +7,6 @@ public class Deck {
     public Deck() {
         deckBuilder(0);
     }
-Random rand = new Random();
     public Card addCard(String value, String suit) {
         return new Card(value, suit);
     }
