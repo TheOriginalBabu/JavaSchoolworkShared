581c45baf765a9c1afa929a84425e9b1fea8e0ca
Griff2005 <griffrob1205@gmail.com>
2023-05-05 11:44:19 -0400

    Update Main.java

--- IzaanGriffinClassesLab/src/PokerHandSimulator/Main.java
+++ IzaanGriffinClassesLab/src/PokerHandSimulator/Main.java
@@ -3,15 +3,19 @@ package PokerHandSimulator;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         long hands;
         while(true) {
             try {
                 System.out.print("Enter the number of hands to be simulated: ");
-                hands = scan.nextInt();
+                hands = Integer.parseInt(scan.nextLine());
+                if (hands < 0) {
+                    throw new Exception();
+                }
                 System.out.println();
-                StatisticAnalyser analyser = new StatisticAnalyser(hands);
+                new StatisticAnalyser(hands);
                 break;
             } catch (Exception e) {
                 System.out.println("Invalid input");
